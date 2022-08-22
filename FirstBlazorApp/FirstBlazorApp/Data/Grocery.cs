9cf418905b341045edfe9fbaae5d6d48f50b9758
Simeon Kirov <simeonkirov21@gmail.com>
2022-08-22 10:20:31 +0300

    Added FirstBlazorApp

---
+++ FirstBlazorApp/FirstBlazorApp/Data/Grocery.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstBlazorApp.Data
+{
+    public class Grocery
+    {
+        [Required]
+        [StringLength(15, ErrorMessage = "Name should be less than 15 characters.")]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 10000, ErrorMessage = "Valid Price Range is (1-10000)")]
+        public float Price { get; set; }
+    }
+}
